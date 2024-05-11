2af263a640ce6b70045674f96707c4ccaf05a26b
Muhammad Huzaifa Ilyas <huzaifailyas02@outlook.com>
2024-05-11 23:32:48 +0500

    Payment Integration with stripe

---
+++ Bolt/src/config/stripe.ts
@@ -0,0 +1,28 @@
+export const PLANS = [
+  {
+    name: "Free",
+    slug: "free",
+    quota: 10,
+    pagesPerPdf: 5,
+    price: {
+      amount: 0,
+      priceId: {
+        test: "",
+        production: "",
+      },
+    },
+  },
+  {
+    name: "Pro",
+    slug: "pro",
+    quota: 50,
+    pagesPerPdf: 25,
+    price: {
+      amount: 13.99,
+      priceId: {
+        test: "price_1PFHnpCgpVnhkvFBPHkWjbFe",
+        production: "",
+      },
+    },
+  },
+];
--- Bolt/src/trpc/index.ts
+++ Bolt/src/trpc/index.ts
@@ -4,6 +4,9 @@ import { TRPCError } from "@trpc/server";
 import { db } from "@/db";
 import { z } from "zod";
 import { INFINTE_QUERY_LIMIT } from "@/config/infinite-query";
+import { absoluteUrl } from "@/lib/utils";
+import { getUserSubscriptionPlan, stripe } from "@/lib/stripe";
+import { PLANS } from "@/config/stripe";
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
@@ -42,6 +45,49 @@ export const appRouter = router({
         userId,
       },
     });
+  }),
+
+  createStripSession: privateProcedure.mutation(async ({ ctx }) => {
+    const { userId } = ctx;
+    const billingUrl = absoluteUrl("/dashboard/billing");
+
+    if (!userId) throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!dbUser) throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    const subscriptionPlan = await getUserSubscriptionPlan();
+
+    if (subscriptionPlan.isSubscribed && dbUser.stripeCustomerId) {
+      const stripeSession = await stripe.billingPortal.sessions.create({
+        customer: dbUser.stripeCustomerId,
+        return_url: billingUrl,
+      });
+      return { url: stripeSession.url };
+    }
+
+    const stripeSession = await stripe.checkout.sessions.create({
+      success_url: billingUrl,
+      cancel_url: billingUrl,
+      payment_method_types: ["card", "paypal"],
+      mode: "subscription",
+      billing_address_collection: "auto",
+      line_items: [
+        {
+          price: PLANS.find((plan) => plan.name === "Pro")?.price.priceId.test,
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        userId: userId,
+      },
+    });
+    return { url: stripeSession.url };
   }),
 
   getFileMessages: privateProcedure
