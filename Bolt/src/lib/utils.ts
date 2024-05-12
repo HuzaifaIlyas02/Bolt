0abc3bcd15033c8cad74019d0f60344a1af33060
Muhammad Huzaifa Ilyas <huzaifailyas02@outlook.com>
2024-05-12 15:39:24 +0500

    Bugs Fixed

--- Bolt/src/lib/utils.ts
+++ Bolt/src/lib/utils.ts
@@ -45,8 +45,8 @@ export function constructMetadata({
       creator: "@m_huzaifa_25",
     },
     icons,
-    metadataBase: new URL("http://localhost:3000/"),
-    themeColor: "",
+    metadataBase: new URL("https://bolt-tau.vercel.app/"),
+    themeColor: "#FFF",
     ...(noIndex && {
       robots: {
         index: false,
